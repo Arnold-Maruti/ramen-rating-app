5f1fbbdbc2daf296239774f877e7c6b0b4d651f0
Arnold <Arnold>
2025-03-20 23:47:27 +0300

    testing addSybmtListener function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "https://moringa.instructure.com/courses/967/files/517801/preview", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "https://moringa.instructure.com/courses/967/files/517800/preview", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "https://moringa.instructure.com/courses/967/files/517798/preview", rating: 3,comment:"awsome" },
-    {id: 4, name:"Naruto Ramen", restaurant:"suzuki", image:"https://moringa.instructure.com/courses/967/files/517799/preview", rating:6, comment:"perfect"},
-    {id:5, name:"kimichi ramen", restaurant:"biko", image:"https://moringa.instructure.com/courses/967/files/517797/preview", rating: 7, comment:"incredible"}
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "./images/tonkotsu.jpeg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "./images/ramen 1.jpeg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "./images/shoyu.jpeg", rating: 3,comment:"awsome" },
+    {id: 4, name:"Naruto Ramen", restaurant:"suzuki", image:"./images/miso.jpeg", rating:6, comment:"perfect"},
+    {id:5, name:"kimichi ramen", restaurant:"biko", image:"./images/kimchi.jpeg", rating: 7, comment:"incredible"}
  ]; 
  
 function main(){
    displayRamen();
    addSubmitListener();
 }
-main();
+document.addEventListener("DOMContentLoaded",main())
 
 
  
@@ -54,7 +54,9 @@ function handleClick(ramen){
 }
 function addSubmitListener(){
    const button=document.getElementById("t62")
-   button.addEventListener('click',()=> submitDetails())
+   button.addEventListener('click',(e)=>{
+       e.preventDefault();
+       submitDetails()})
 }
 
 function submitDetails(){
@@ -74,7 +76,7 @@ function submitDetails(){
    
 
    const newArray={
-      id: ramens.lenght+1,
+      id: ramens.length+1,
       name:name_value,
       restaurant: restaurant_value,
       image: image_value,
@@ -85,10 +87,6 @@ function submitDetails(){
 
    ramens.push(newArray);
 
-   displayRamen();
-   
-   console.log(name_value);
-   ;
-
-
-}
+   displayRamen(ramens);
+   document.querySelector("#ramen_form").reset();
+    }
